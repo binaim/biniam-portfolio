f34f95908c0068b4b5c99efe4d059f8bdc2df0ae
Biniam Gheb <luwyzb@gmail.com>
2024-01-27 01:22:53 -0800

    predeploy test

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function SocialMedia() {
+  return (
+    <ul className="home-about-social-links">
+      <li className="social-icons">
+        <a
+          href="https://github.com/binaim/SCADA-AUTOMATION-SERVICES"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour  home-social-icons"
+        >
+          <AiFillGithub className="social-icon" />
+        </a>
+      </li>
+      <li className="social-icons">
+        <a
+          href="https://www.linkedin.com/in/biniam-ghebremedhin-244311235/"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour  home-social-icons"
+        >
+          <FaLinkedinIn className="social-icon" />
+        </a>
+      </li>
+      <li className="social-icons">
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour home-social-icons"
+        >
+          <AiFillInstagram className="social-icon" />
+        </a>
+      </li>
+      <li className="social-icons">
+        <a
+          href="mailto:biniamgg11@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour home-social-icons"
+        >
+          <MdEmail className="social-icon" />
+        </a>
+      </li>
+    </ul>
+  );
+}
+export default SocialMedia;
